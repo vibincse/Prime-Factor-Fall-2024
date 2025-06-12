936a1b3fba3cdc240bd70ef9e0167eb61dbd24ac
vibin Rajan <vibin.cse@gmail.com>
2025-06-11 21:38:42 -0400

    Return [2, 2, 2] for input 8
    Return [2, 2, 2] for input 8

--- test_prime.py
+++ test_prime.py
@@ -26,3 +26,7 @@ def test_prime_factors_of_four_returns_two_twice():
 
 def test_prime_factors_of_six_returns_two_and_three():
     assert generate_prime_factors(6) == [2, 3]
+
+
+def test_prime_factors_of_eight_returns_three_twos():
+    assert generate_prime_factors(8) == [2, 2, 2]
